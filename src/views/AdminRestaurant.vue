04bf71f4f795a7ff2491bd9dd9fdd16919f4c82e
TomatoSoup <jonathantang0126@gmail.com>
2020-03-17 14:31:46 +0800

    finish adminRestaurant api connect

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -41,27 +41,9 @@
 </template>
 <script>
 import { emptyImageFilter } from './../utils/mixins'
-const dummyData = {
-  restaurant: {
-    id: 2,
-    name: 'Mrs. Mckenzie Johnston',
-    tel: '567-714-6131 x621',
-    address: '61371 Rosalinda Knoll',
-    opening_hours: '08:00',
-    description:
-      'Quia pariatur perferendis architecto tenetur omnis pariatur tempore.',
-    image: 'https://loremflickr.com/320/240/food,dessert,restaurant/?random=2',
-    createdAt: '2019-06-22T09:00:43.000Z',
-    updatedAt: '2019-06-22T09:00:43.000Z',
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  }
-}
+import adminAPI from '../apis/admin'
+import { Toast } from '../utils/helpers'
+
 export default {
   name: 'AdminRestaurant',
   mixins: [emptyImageFilter],
@@ -79,14 +61,33 @@ export default {
       }
     }
   },
+
   mounted () {
     const { id: restaurantId } = this.$route.params
+
     this.fetchRestaurant(restaurantId)
   },
+
+  beforeRouteUpdate (to, from, next) {
+    // 路由改變時重新抓取資料
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
+  },
+
   methods: {
-        // eslint-disable-next-line
-    fetchRestaurant (restaurantId) {
-      const { restaurant } = dummyData
+     
+  async fetchRestaurant (restaurantId) {
+   
+    try {
+
+      const {data, statusText} = await adminAPI.restaurant.getDetail({restaurantId})
+
+      if (statusText !== 'OK') {
+        throw new Error(statusText)
+      }
+
+      const { restaurant } = data
       this.restaurant = {
         ...this.restaurant,
         id: restaurant.id,
@@ -98,6 +99,19 @@ export default {
         address: restaurant.address,
         description: restaurant.description
       }
+      
+      
+    } catch (error) {
+      
+      Toast.fire({
+        icon:'error',
+        title: '無法取得資料，請稍後再試'
+      })
+
+    }
+
+
+
     }
   }
 }
